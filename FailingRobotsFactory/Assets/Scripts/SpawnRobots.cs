599547e7fa6fc442a37c6352f80014d353cb8723
Sykkoss <damien.cosson@epitech.eu>
2019-09-30 00:02:48 +0900

    Basics robots can spawn indefinitly. Moving ones will spawn outside of the screen and still ones within screen borders. They all can spawn on 3 axis and can't spawn onto another robot

--- FailingRobotsFactory/Assets/Scripts/SpawnRobots.cs
+++ FailingRobotsFactory/Assets/Scripts/SpawnRobots.cs
@@ -7,66 +7,118 @@ public class SpawnRobots : MonoBehaviour
     public int maxRobots;
     public GameObject basicRobotPrefab;
 
-    public int actualRobots;
-
-    void Start()
-    {
-        actualRobots = 0;
-    }
-
     void Update()
     {
        if (gameObject.transform.childCount < maxRobots)
        {
-           SpawnRobot(); // Argument with random between 1 -> 3
+           SpawnRobot(); //// Argument with random between 1 -> 3
        }
     }
 
     private void SpawnRobot()
     {
-        // Later check on how many random got
-        SpawnBasicRobot(Random.Range(0, 101));
+        //// Later check on how many random got
+        SpawnBasicRobot(Random.Range(0, 100));
     }
     
     private void SpawnBasicRobot(int rand)
     {
-        float ySpawn = GetYSpawn();
+        float ySpawn = GenerateYSpawn();
+        float xSpawn;
+        BasicRobot.AIBasicRobot movementType;
 
-        if (rand <= 75)
+        if (rand < 90) // 90% chance to create a moving robot
         {
             if (Random.Range(0, 2) == 0)
-                CreateBasicRobot(BasicRobot.AIBasicRobot.MOVE_LEFT, ySpawn);
+                movementType = BasicRobot.AIBasicRobot.MOVE_LEFT;
             else
-                CreateBasicRobot(BasicRobot.AIBasicRobot.MOVE_RIGHT, ySpawn);
+                movementType = BasicRobot.AIBasicRobot.MOVE_RIGHT;
+            xSpawn = GenerateXSpawnMoving(movementType, ySpawn);
+            if (!float.IsNaN(xSpawn))
+                CreateBasicRobot(movementType, xSpawn, ySpawn);
         }
         else
-            CreateBasicRobot(BasicRobot.AIBasicRobot.STILL, ySpawn);
-        actualRobots += 1;
+        {
+            xSpawn = GenerateXSpawnStill(ySpawn);
+            if (!float.IsNaN(xSpawn))
+                CreateBasicRobot(BasicRobot.AIBasicRobot.STILL, xSpawn, ySpawn);
+        }
     }
 
-    public BasicRobot CreateBasicRobot(BasicRobot.AIBasicRobot ai, float ySpawn)
+    public BasicRobot CreateBasicRobot(BasicRobot.AIBasicRobot ai, float xSpawn, float ySpawn)
     {
         GameObject newGo = Instantiate(basicRobotPrefab) as GameObject;
         BasicRobot newRobot = newGo.GetComponent<BasicRobot>();
-        
-        newRobot.transform.position = new Vector3(newRobot.transform.position.x, ySpawn, newRobot.transform.position.z);
+
+        newRobot.transform.position = new Vector3(xSpawn, ySpawn, newRobot.transform.position.z);
         newRobot.transform.parent = transform;
         newRobot.AIRobot = ai;
         return newRobot;
     }
 
-    private float GetYSpawn()
+    /* Generates a random X position on borders of the screen*/
+    private float GenerateXSpawnMoving(BasicRobot.AIBasicRobot movementType, float ySpawn)
+    {
+        Camera camera = Camera.main;
+        float halfHeight = camera.orthographicSize;
+        float halfWidth = camera.aspect * halfHeight;
+        float leftSide = -halfWidth - (basicRobotPrefab.transform.localScale.x * 10); // Multplying by 10 allows the robot not to be too close to screen borders
+        float rightSide = halfWidth + (basicRobotPrefab.transform.localScale.x * 10);
+
+        if (movementType == BasicRobot.AIBasicRobot.MOVE_LEFT)
+        {
+            if (CheckSpawnOnOther(rightSide, ySpawn) == true)
+                return float.NaN;
+            return rightSide;
+        }
+        else
+        {
+            if (CheckSpawnOnOther(leftSide, ySpawn) == true)
+                return float.NaN;
+            return leftSide;
+        }
+    }
+
+    /* Generates a random X position within screen size */
+    float GenerateXSpawnStill(float ySpawn)
+    {
+        Camera camera = Camera.main;
+        float halfHeight = camera.orthographicSize;
+        float halfWidth = camera.aspect * halfHeight;
+        float minWidth = -halfWidth + (basicRobotPrefab.transform.localScale.x * 10); // Multplying by 10 allows the robot not to be too close to screen borders
+        float maxWidth = halfWidth - (basicRobotPrefab.transform.localScale.x * 10);
+        float xSpawn = Random.Range(minWidth, maxWidth);
+
+        if (CheckSpawnOnOther(xSpawn, ySpawn) == true)
+            return float.NaN;
+        return xSpawn;
+    }
+
+    private float GenerateYSpawn()
     {
         GameParameters gameParameters = FindObjectOfType<GameParameters>();
-        float randomYPos = Random.Range(0, 101);
+        float randomYPos = Random.Range(0, 100);
         float yPos;
 
-        if (randomYPos <= 33)
+        if (randomYPos < 33)
             yPos = gameParameters.ySpawn1;
-        else if (randomYPos <= 66)
+        else if (randomYPos < 66)
             yPos = gameParameters.ySpawn2;
         else
             yPos = gameParameters.ySpawn3;
         return yPos;
+    }
+
+    private bool CheckSpawnOnOther(float xSpawn, float ySpawn)
+    {
+        float childScale;
+
+        foreach (Transform child in transform)
+        {
+            childScale = child.localScale.x * 15; // Multplying by 15 allows robots not to be too close to each others
+            if (ySpawn == child.position.y && (child.position.x - childScale <= xSpawn && child.position.x + childScale >= xSpawn))
+                return true;
+        }
+        return false;
     }
 }
