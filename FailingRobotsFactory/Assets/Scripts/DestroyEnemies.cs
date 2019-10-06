da171455297193e52934e0566b4354ed65368440
Sykkoss <damien.cosson@epitech.eu>
2019-10-06 14:21:03 +0900

    fix: Multiple kill with single touch #1

--- FailingRobotsFactory/Assets/Scripts/DestroyEnemies.cs
+++ FailingRobotsFactory/Assets/Scripts/DestroyEnemies.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class DestroyEnemies : MonoBehaviour
 {
-    private RaycastHit2D hit;
-
     private void Update()
     {
-        for (int input = 0; input < Input.touchCount; input++)
+        if (Input.touchCount > 0)
+            KillRobotOnTouch();
+    }
+
+    private void KillRobotOnTouch()
+    {
+        Touch actualTouch = Input.GetTouch(0);
+        RaycastHit2D hit;
+
+        if (actualTouch.phase == TouchPhase.Began)
         {
-            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(input).position), Vector2.zero);
+            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(actualTouch.position), Vector2.zero);
 
             if (hit.transform != null)
                 Destroy(hit.transform.gameObject);
         }
     }
 }
+
