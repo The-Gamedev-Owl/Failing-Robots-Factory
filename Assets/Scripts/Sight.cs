9b23f7a5d3a41e1ac897f785b566ff3d4f2cae4d
Sykkoss <damien.cosson@epitech.eu>
2019-10-26 16:52:04 +0900

    feat: Implement Player sight

--- Assets/Scripts/Sight.cs
+++ Assets/Scripts/Sight.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Sight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float minimumAlphaMap;
+    public float timeBeforeFade;
+    public float fadeOutDuration;
+    public Material mapFadeOutMat;
+    public Material robotsFadeOutMat;
+
+    private Coroutine fadeOutCoroutine;
+
+    private void Start()
     {
-        
+        fadeOutCoroutine = null;
+        ResetFade();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetFade()
     {
-        
+        mapFadeOutMat.color = Color.white;
+        robotsFadeOutMat.color = Color.white;
+        if (fadeOutCoroutine != null)
+            StopCoroutine(fadeOutCoroutine);
+        fadeOutCoroutine = StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float startTime;
+        float timeStep;
+        float newAlphaRobot;
+        float newAlphaMap;
+
+        yield return new WaitForSeconds(timeBeforeFade);
+        startTime = Time.time;
+        while (mapFadeOutMat.color.a > 0)
+        {
+            timeStep = (Time.time - startTime) / fadeOutDuration;
+            newAlphaRobot = Mathf.SmoothStep(1f, 0f, timeStep);
+            newAlphaMap = Mathf.SmoothStep(1f, minimumAlphaMap, timeStep);
+            mapFadeOutMat.color = new Color(1f, 1f, 1f, newAlphaMap);
+            robotsFadeOutMat.color = new Color(1f, 1f, 1f, newAlphaRobot);
+            yield return null;
+        }
     }
 }
